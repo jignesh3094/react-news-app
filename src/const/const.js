b236472cf5c88afd6bd85fe9f7b05135a85db41a
jignesh patel <jignesh3094@gmail.com>
2024-04-12 11:24:32 +0530

    changes for post

--- src/const/const.js
+++ src/const/const.js
@@ -1,5 +1,6 @@
 
-// export const api_url = 'https://nodejs-mjyz.onrender.com/';
+export const api_url = 'https://nodejs-mjyz.onrender.com/';
+export const imageFolderPath = 'https://nodejs-mjyz.onrender.com/';
 
-export const api_url = 'http://localhost:3000/';
-export const imageFolderPath = 'http://localhost:3000/images/'
+// export const api_url = 'http://localhost:3000/';
+// export const imageFolderPath = 'http://localhost:3000/images/'
